ce9cf8267a9d36ab95d1a81ce4f9d9d34f6b054b
Shyish <shyballano@gmail.com>
2018-11-21 16:16:21 +0100

    wip

--- data/src/androidTest/java/com/ricamgar/notify/data/AppDatabaseTest.kt
+++ data/src/androidTest/java/com/ricamgar/notify/data/AppDatabaseTest.kt
@@ -2,8 +2,18 @@ package com.ricamgar.notify.data
 
 import android.arch.persistence.db.framework.FrameworkSQLiteOpenHelperFactory
 import android.arch.persistence.room.testing.MigrationTestHelper
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import androidx.test.platform.app.InstrumentationRegistry
 import com.ricamgar.notify.data.database.AppDatabase
+import com.ricamgar.notify.data.database.MIGRATION_1_2
+import com.ricamgar.notify.data.database.MIGRATION_2_3
+import com.zdvdev.kursor.toFormattedString
+import org.junit.Assert.assertEquals
 import org.junit.Rule
+import org.junit.Test
+
+private const val TEST_DB_NAME = "test_db"
 
 class AppDatabaseTest {
   @Rule val testHelper: MigrationTestHelper = MigrationTestHelper(
@@ -11,4 +21,23 @@ class AppDatabaseTest {
     AppDatabase::class.java.canonicalName,
     FrameworkSQLiteOpenHelperFactory()
   )
+
+  @Test fun shouldPerformRemindersMigrationsCorrectly() {
+    val cursor = testHelper.createDatabase(TEST_DB_NAME, 1).use {
+      it.insert("reminders", SQLiteDatabase.CONFLICT_FAIL, ContentValues().apply {
+        put("id", 1)
+        put("description", "description")
+        put("done", true)
+        put("groupId", 99)
+      })
+      it.query("SELECT * FROM reminders")
+    }
+
+    assertEquals(
+      cursor.toFormattedString(),
+      testHelper.runMigrationsAndValidate(TEST_DB_NAME, 4, true, MIGRATION_1_2, MIGRATION_2_3).use {
+        it.query("SELECT * FROM reminders")
+      }.toFormattedString()
+    )
+  }
 }
